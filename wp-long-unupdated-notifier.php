c71a940e76f0c32da468b33f15d0183345e21091
DESKTOP-FH7LPHE\wynes <wynes@protonmail.com>
2023-08-19 19:16:17 +0900

    first commit

---
+++ wp-long-unupdated-notifier.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: WP Long Unupdated Notifier
+  Plugin URI: https://github.com/saaria/wp-long-unupdated-notifier
+  Description: The user is notified at the beginning of the article body that the content of the article is out of date.
+  Version: 1.0.0
+  Author: YUKI
+  Author URI: https://github.com/saaria/
+  License: GPLv2
+ */
